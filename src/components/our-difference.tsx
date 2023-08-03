dac39d1410ccabbb393467a6f5d4fa8238ed3ba8
Mohammad Aqib Yatoo <Mohammad.Yatoo@Solarisgroup.com>
2023-08-03 20:42:30 +0530

    add demo dections

--- src/components/our-difference.tsx
+++ src/components/our-difference.tsx
@@ -1,9 +1,14 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Show } from "@chakra-ui/react";
 
 export const OurDifference = () => {
   return (
     <Box id="section__our_difference">
-      <Image src="/what-makes-us-different.jpg" alt="our difference" />
+      <Show above="sm">
+        <Image src="/what-makes-us-different.jpg" alt="our difference" />
+      </Show>
+      <Show below="sm">
+        <Image src="/our-difference-mobile.png" alt="our difference" />
+      </Show>
     </Box>
   );
 };
