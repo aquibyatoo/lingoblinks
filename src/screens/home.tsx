dbda1975dd1376a6ff8fc9d410e7d5742aec4467
Mohammad Aqib Yatoo <Mohammad.Yatoo@Solarisgroup.com>
2023-08-08 22:20:28 +0530

    feat: add contect section
    Signed-off-by: Mohammad Aqib Yatoo <Mohammad.Yatoo@Solarisgroup.com>

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -4,6 +4,7 @@ import { DemoSection2 } from "../components/demo-section-2";
 import { DemoSection3 } from "../components/demo-section-3";
 import { Footer } from "../components/footer";
 import { MainHero } from "../components/main-hero";
+import { ConnectWithUs } from "../components/connect-with-us";
 
 export const HomeScreen = () => {
   return (
@@ -12,6 +13,7 @@ export const HomeScreen = () => {
       <DemoSection1 />
       <DemoSection2 />
       <DemoSection3 />
+      <ConnectWithUs />
       <Footer />
     </Box>
   );
