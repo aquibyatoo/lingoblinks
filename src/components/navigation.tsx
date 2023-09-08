30c520d8ed0b283d7e10ecb4c72b85166c444fc1
Mohammad Aqib Yatoo <Mohammad.Yatoo@Solarisgroup.com>
2023-09-08 18:02:01 +0530

    adjusted links
    Signed-off-by: Mohammad Aqib Yatoo <Mohammad.Yatoo@Solarisgroup.com>

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,37 +9,42 @@ export const Navigation = () => {
         direction={"row"}
         spacing={6}
       >
-        <Button as={"a"} fontWeight={400} variant={"link"} href={"/"}>
+        <Button as={"a"} colorScheme="black" variant={"link"} href={"/"}>
           Home
         </Button>
         <Button
           as={"a"}
-          fontWeight={400}
           variant={"link"}
+          colorScheme="black"
           href={"/our-difference"}
         >
           What Makes Us Different
         </Button>
-        <Button as={"a"} fontWeight={400} variant={"link"} href={"/about"}>
+        <Button as={"a"} variant={"link"} colorScheme="black" href={"/about"}>
           About
         </Button>
         <Button
           as={"a"}
-          fontWeight={400}
           variant={"link"}
+          colorScheme="black"
           href={"#connect__with_us_section"}
         >
           Contact
         </Button>
         <Button
           as={"a"}
-          fontWeight={400}
           variant={"link"}
+          colorScheme="black"
           href={"/our-mission"}
         >
           Mission
         </Button>
-        <Button as={"a"} fontWeight={400} variant={"link"} href={"/support-us"}>
+        <Button
+          as={"a"}
+          variant={"link"}
+          colorScheme="black"
+          href={"/support-us"}
+        >
           Support Us
         </Button>
       </Stack>
